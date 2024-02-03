082bc04734af696204fb9f165c3cef46bcdf308f
Samuel Moelius <sam@moeli.us>
2024-02-02 20:16:34 -0500

    Allow running one test at a time

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use std::{
+    env::var_os,
     ffi::OsStr,
     fs::{copy, create_dir, read_dir, read_to_string, write},
     path::Path,
@@ -10,31 +11,43 @@ mod util;
 
 #[test]
 fn integration() {
-    for entry in read_dir("tests/cases").unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
+    let test_cases = Path::new("tests/cases");
 
-        if path.extension() != Some(OsStr::new("rs")) {
-            continue;
+    if let Some(testname) = var_os("TESTNAME") {
+        test_case(&test_cases.join(testname).with_extension("rs"));
+    } else {
+        for entry in read_dir("tests/cases").unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+
+            if path.extension() != Some(OsStr::new("rs")) {
+                continue;
+            }
+
+            test_case(&path);
         }
-
-        let stderr_path = path.with_extension("stderr");
-        let expected_stderr_substring = read_to_string(stderr_path).unwrap();
-
-        let temp_package = temp_package(&path).unwrap();
-
-        let mut command = util::build_with_build_wrap();
-        command.current_dir(&temp_package);
-
-        let output = util::exec(command, false).unwrap();
-        assert_eq!(
-            expected_stderr_substring.is_empty(),
-            output.status.success()
-        );
-
-        let stderr = std::str::from_utf8(&output.stderr).unwrap();
-        assert!(stderr.contains(expected_stderr_substring.trim_end()));
     }
+}
+
+fn test_case(path: &Path) {
+    let stderr_path = path.with_extension("stderr");
+    let expected_stderr_substring = read_to_string(stderr_path).unwrap();
+
+    let temp_package = temp_package(&path).unwrap();
+
+    let mut command = util::build_with_build_wrap();
+    command.current_dir(&temp_package);
+
+    let output = util::exec(command, false).unwrap();
+    assert_eq!(
+        expected_stderr_substring.is_empty(),
+        output.status.success(),
+        "{path:?} failed in {:?}",
+        temp_package.into_path()
+    );
+
+    let stderr = std::str::from_utf8(&output.stderr).unwrap();
+    assert!(stderr.contains(expected_stderr_substring.trim_end()));
 }
 
 fn temp_package(build_script_path: &Path) -> Result<TempDir> {
