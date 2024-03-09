f014a1c5da6b425cf5e26c799d5c33d83d25f195
Samuel Moelius <sam@moeli.us>
2024-03-09 06:19:27 -0500

    Add `clippy` test

---
+++ tests/ci.rs
@@ -0,0 +1,16 @@
+use assert_cmd::Command;
+
+#[test]
+fn clippy() {
+    Command::new("cargo")
+        .args([
+            "+nightly",
+            "clippy",
+            "--all-features",
+            "--all-targets",
+            "--",
+            "--deny=warnings",
+        ])
+        .assert()
+        .success();
+}
