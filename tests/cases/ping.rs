a2412e8a814f53b6746c4bedb16ff81463f53f29
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-03-18 19:11:18 +0000

    Enable `ping` verbose output

--- tests/cases/ping.rs
+++ tests/cases/ping.rs
@@ -2,7 +2,7 @@ use std::process::{exit, Command, ExitStatus};
 
 fn main() {
     let status: ExitStatus = Command::new("ping")
-        .args(["-c", "1", "127.0.0.1"])
+        .args(["-c", "1", "-v", "127.0.0.1"])
         .status()
         .unwrap();
     let code = status.code().unwrap();
