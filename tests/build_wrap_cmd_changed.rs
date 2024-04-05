7f2f5d717873d1f3bdc445b8f55770b61bd12a1e
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-04-04 20:47:32 -0400

    Improve `build_wrap_cmd_changed` error messages

--- tests/build_wrap_cmd_changed.rs
+++ tests/build_wrap_cmd_changed.rs
@@ -40,9 +40,15 @@ fn build_wrap_cmd_changed() {
 
 fn exec_and_check_stdout(command: Command, should_contain: bool, needle: &str) {
     let output = util::exec(command, false).unwrap();
-    assert!(output.status.success());
-
     let stderr = std::str::from_utf8(&output.stderr).unwrap();
+    assert!(
+        output.status.success(),
+        "command failed:\n```\n{stderr}\n```"
+    );
+
     let contains = stderr.contains(needle);
-    assert_eq!(should_contain, contains);
+    assert_eq!(
+        should_contain, contains,
+        "unexpected stderr contents:\n```\n{stderr}\n```"
+    );
 }
