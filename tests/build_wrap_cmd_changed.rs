fd0a17f498a7f95bf0935e3a2143b3de998490d3
Samuel Moelius <sam@moeli.us>
2024-05-06 04:47:31 -0400

    Correct function name in `build_wrap_cmd_changed` test

--- tests/build_wrap_cmd_changed.rs
+++ tests/build_wrap_cmd_changed.rs
@@ -12,7 +12,7 @@ fn build_wrap_cmd_changed() {
     let mut command = util::build_with_build_wrap();
     command.current_dir(&temp_package);
 
-    exec_and_check_stdout(command, false, "] real ");
+    exec_and_check_stderr(command, false, "] real ");
 
     // smoelius: Build with `BUILD_WRAP_CMD` set to `time -p {}`.
 
@@ -20,7 +20,7 @@ fn build_wrap_cmd_changed() {
     command.env("BUILD_WRAP_CMD", "time -p {}");
     command.current_dir(&temp_package);
 
-    exec_and_check_stdout(command, false, "] real ");
+    exec_and_check_stderr(command, false, "] real ");
 
     // smoelius: Clean and build again with `BUILD_WRAP_CMD` set to `time -p {}`.
 
@@ -35,10 +35,10 @@ fn build_wrap_cmd_changed() {
     command.env("BUILD_WRAP_CMD", "time -p {}");
     command.current_dir(&temp_package);
 
-    exec_and_check_stdout(command, true, "] real ");
+    exec_and_check_stderr(command, true, "] real ");
 }
 
-fn exec_and_check_stdout(command: Command, should_contain: bool, needle: &str) {
+fn exec_and_check_stderr(command: Command, should_contain: bool, needle: &str) {
     let output = util::exec_forwarding_output(command, false).unwrap();
     let stderr = std::str::from_utf8(&output.stderr).unwrap();
     assert!(
