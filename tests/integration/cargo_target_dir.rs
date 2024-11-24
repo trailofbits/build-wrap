dd2c4d25c933c037f8950fd38d3b3f0fb227245d
Samuel Moelius <sam@moeli.us>
2024-11-24 09:18:22 -0500

    `cargo_config` -> `cargo_target_dir`

--- tests/integration/cargo_target_dir.rs
+++ tests/integration/cargo_target_dir.rs
@@ -1,18 +1,18 @@
 use crate::util;
 use std::{path::Path, process::Command};
 
-const DIR: &str = "fixtures/cargo_config";
+const DIR: &str = "fixtures/cargo_target_dir";
 
 #[test]
-fn cargo_config() {
+fn cargo_target_dir() {
     let command = util::build_with_default_linker();
     test_build(command);
 
     // smoelius: When `build-wrap` builds the wrapper package, it expects the target directory to be
-    // `target`. So building the wrapper package in `fixtures/cargo_config` would fail because it
-    // contains a `.cargo/config.toml` that sets the target directory to `target-custom`.
+    // `target`. So building the wrapper package in `fixtures/cargo_target_dir` would fail because
+    // it contains a `.cargo/config.toml` that sets the target directory to `target-custom`.
 
-    // smoelius: The build script in `fixtures/cargo_config` prints the path of the current
+    // smoelius: The build script in `fixtures/cargo_target_dir` prints the path of the current
     // executable, i.e., the wrapped, original build script. Previously, this was unpacked into
     // `std::env::temp_dir()`. However, `build-wrap` now renames the original build script so that
     // it is a sibling of the wrapper build script. Hence, when this test is run, the current
@@ -21,7 +21,7 @@ fn cargo_config() {
     test_build(command);
 
     // smoelius: Building the wrapper package should similarly succeed if `CARGO_TARGET_DIR` is set.
-    // Note that the `fixtures/cargo_config` directory must be cleaned for it to be rebuilt.
+    // Note that the `fixtures/cargo_target_dir` directory must be cleaned for it to be rebuilt.
     let status = Command::new("cargo")
         .arg("clean")
         .current_dir(DIR)
@@ -42,7 +42,7 @@ fn test_build(mut command: Command) {
         .join(DIR)
         .join("target-custom/debug");
     assert!(stderr.lines().any(|line| line.starts_with(&format!(
-        "warning: cargo_config@0.1.0: {}/",
+        "warning: cargo_target_dir@0.1.0: {}/",
         trim_trailing_slashes(&expected_dir.to_string_lossy())
     ))));
 }
