d0d4daeb8a714e47bea433fbf4a188fe15fbc525
Samuel Moelius <sam@moeli.us>
2025-09-16 12:40:26 -0400

    Fix tests

--- tests/integration/config.rs
+++ tests/integration/config.rs
@@ -3,6 +3,7 @@ use serde::Deserialize;
 use std::{
     env::{consts, var_os},
     fs::{read_dir, read_to_string},
+    io::Write,
     path::Path,
 };
 
@@ -19,24 +20,31 @@ pub fn for_each_test_case(
     dir: impl AsRef<Path>,
     f: impl Fn(Option<&str>, &Path, &str) -> Result<()>,
 ) -> Result<()> {
-    let mut dirs = Vec::new();
+    let mut subdirs = Vec::new();
     let mut test_cases = Vec::new();
 
     for result in read_dir(dir)? {
         let entry = result?;
         let path = entry.path();
         if path.is_dir() {
-            dirs.push(path);
+            subdirs.push(path);
         } else {
             test_cases.push(path);
         }
     }
 
-    dirs.sort();
+    subdirs.sort();
     test_cases.sort();
 
-    for dir in dirs {
-        let config_toml_path = dir.join("config.toml");
+    // smoelius: Each `subdir` should contain:
+    //
+    // - a config.toml file encoding a `Config`
+    // - a stderr file for each test case found in `dir`
+    //
+    // If a stderr file cannot be found for a test case in `dir`, a warning is issued but it does
+    // not cause the test to fail.
+    for subdir in subdirs {
+        let config_toml_path = subdir.join("config.toml");
         let contents = read_to_string(&config_toml_path)
             .with_context(|| format!("failed to read `{}`", config_toml_path.display()))?;
         let config = toml::from_str::<Config>(&contents)?;
@@ -52,7 +60,17 @@ pub fn for_each_test_case(
                     return Ok(());
                 }
             }
-            let stderr_path = dir.join(file_stem).with_extension("stderr");
+            let stderr_path = subdir.join(file_stem).with_extension("stderr");
+            if !stderr_path.try_exists()? {
+                #[allow(clippy::explicit_write)]
+                writeln!(
+                    std::io::stderr(),
+                    "warning: `{}` does not exist",
+                    stderr_path.display()
+                )
+                .unwrap();
+                continue;
+            }
             let stderr = read_to_string(&stderr_path)
                 .with_context(|| format!("failed to read `{}`", stderr_path.display()))?;
             f(config.build_wrap_cmd.as_deref(), test_case, &stderr)?;
