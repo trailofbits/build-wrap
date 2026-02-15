6ba825485488a1c0ec5f02a21cff0a19338028b7
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-02-15 18:34:25 -0500

    Add deadline flag to `ping`

--- tests/build_scripts/ping.rs
+++ tests/build_scripts/ping.rs
@@ -1,8 +1,14 @@
-use std::process::{exit, Command, ExitStatus};
+use std::process::{Command, ExitStatus, exit};
+
+const DEADLINE_FLAG: &str = if cfg!(target_os = "linux") {
+    "-w"
+} else {
+    "-t"
+};
 
 fn main() {
     let status: ExitStatus = Command::new("ping")
-        .args(["-c", "1", "-v", "127.0.0.1"])
+        .args(["-c", "1", DEADLINE_FLAG, "1", "-v", "127.0.0.1"])
         .status()
         .unwrap();
     let code = status.code().unwrap();
